9da9feeb2f549857630720bfdf4455f8c2fd2da6
Michael Mullet <Michaelmullet007@gmail.com>
2021-05-27 18:55:49 -0400

    Add comments

--- MITx Class/Unit 1/problemSet1Problem3.py
+++ MITx Class/Unit 1/problemSet1Problem3.py
@@ -1,17 +1,21 @@
-count = 0
-maxCount = 0
-result = 0
-s = 'bababcbcde'
+count = 0 # Variable that counts the current number of successive alphabetical letters
+maxCount = 0 # Variable that counts the highest number of successive alphabetical letters
+lastCount = 0 # Variable that indexes the last letter of the longest string in s
+s = input('Please type a string of letters: ') # Letters typed by the user for the program to operate on
 
-for i in range(len(s)-1):
-    if s[i] <= s[i+1]:
-        count += 1
-        if count > maxCount:
-            maxCount = count
-            result = i+1
-    else:
-        count = 0
+for i in range(len(s)-1): # Program loop that iterates over the number of letters in s
+    try:
+        if s[i] <= s[i+1]: # If the current letter is <= the next letter (letters have a numerical value)
+            count += 1 # Count variable goes up by one
+            if count > maxCount: # If count is greater than maxCount
+                maxCount = count # Then maxCount is now count
+                lastCount = i+1 # And the index of the last letter for maxCount is moved over one
+        else: # If the current letter is less than the next letter
+            count = 0 # Count is cleared, ready to start over in the next loop
+    except ValueError:
+        print('Please type a valid string.')
 
-longest = s[result-maxCount:result+1]
 
-print ("Longest substring in alphabetical order is: " + longest)
+longest = s[lastCount-maxCount:lastCount+1] # The linked letters mapped to the maxCount in s
+
+print ("Longest substring in alphabetical order is: " + longest) # The outcome of the program
